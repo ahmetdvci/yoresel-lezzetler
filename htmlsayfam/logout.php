7752a764dbadcbdbd97878ba0068bbd4ae2e4c96
Ahmet Nuri <deveciahmetnuri@outlook.com>
2025-01-11 22:48:59 +0300

    Delete htmlsayfam directory

--- htmlsayfam/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Tüm oturum değişkenlerini kaldır
-session_destroy(); // Oturumu sonlandır
-header("Location: index.php"); // Ana sayfaya yönlendir
-exit;
-?>
